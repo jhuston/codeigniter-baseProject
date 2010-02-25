210763224f0a95eae66a37841c3a689f32125e13
Jeff Huston <huston.jeff@gmail.com>
2010-02-25 13:51:21 -0800

    base codeigniter project

--- system/application/config/config.php
+++ system/application/config/config.php
@@ -23,7 +23,7 @@ $config['base_url']	= "http://example.com/";
 | variable so that it is blank.
 |
 */
-$config['index_page'] = "index.php";
+$config['index_page'] = "";
 
 /*
 |--------------------------------------------------------------------------
