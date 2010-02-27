3cd898574c44d860406a4b90beeb8c109ca5cde5
Jeff Huston <huston.jeff@gmail.com>
2010-02-26 23:21:19 -0800

    added base MY_Model and stubbed controller methods

---
+++ system/application/controllers/newController.php
@@ -0,0 +1,44 @@
+<?php
+
+class NewController extends MY_Controller{
+	
+	public function __construct() {
+		parent::__construct();
+		
+	}
+	
+	public function index()
+	{
+		# code...
+	}
+	
+	public function show()
+	{
+	
+	}
+	
+	public function new()
+	{
+		# code...
+	}
+	
+	public function edit()
+	{
+		# code...
+	}
+	
+	public function create()
+	{
+		# code...
+	}
+
+	public function update()
+	{
+		# code...
+	}
+
+	public function destroy()
+	{
+		# code...
+	}
+}
